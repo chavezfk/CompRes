f57715c0f454080e17cd3646583e63ff0405518c
Franz Chavez <chavezfk@gmail.com>
2017-04-07 11:06:35 -0600

    add scripts api, but it isnt used. and add comments to setup

--- sheets_setup.js
+++ sheets_setup.js
@@ -2,11 +2,11 @@
 var CLIENT_ID = '258445166474-80ujp3mlomrme7tmctrf8fmfsqlpi0ch.apps.googleusercontent.com';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4","https://script.googleapis.com/$discovery/rest?version=v1"];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive";
 
 /**
 *  On load, called to load the auth2 library and API client library.
@@ -20,15 +20,15 @@ gapi.load('client:auth2', initClient);
 *  listeners.
 */
 function initClient() {
-gapi.client.init({
-  discoveryDocs: DISCOVERY_DOCS,
-  clientId: CLIENT_ID,
-  scope: SCOPES
-}).then(function () {
-  if(gapi.auth2.getAuthInstance().isSignedIn.get()){
-      
-  }else{
-      gapi.auth2.getAuthInstance().signIn();
-  }
-});
+    gapi.client.init({
+    discoveryDocs: DISCOVERY_DOCS,
+    clientId: CLIENT_ID,
+    scope: SCOPES
+    }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
 }
